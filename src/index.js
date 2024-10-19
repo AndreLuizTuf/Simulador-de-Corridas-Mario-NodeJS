2899078656de1c10ad9a896f594c7790ba548052
AndréLuizTuf <andre.luiz.a.f@hotmail.com>
2024-10-19 09:56:50 -0300

    Objetos e images

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Definindo os jogadores
+const player1 = {
+    Nome: "Mario", // Nome do jogador 1
+    VELOCIDADE: 4,   // Velocidade do Mario
+    MANOBRABILIDADE: 3, // Nível de habilidade de manobras do Mario
+    PODER: 3,         // Nível de poder do Mario
+    PONTOS: 0        // Pontuação inicial do Mario
+};
+
+const player2 = {
+    Nome: "Luigi", // Nome do jogador 2
+    VELOCIDADE: 5,   // Velocidade do Luigi
+    MANOBRABILIDADE: 2, // Nível de habilidade de manobras do Luigi
+    PODER: 2,         // Nível de poder do Luigi
+    PONTOS: 0        // Pontuação inicial do Luigi
+}
