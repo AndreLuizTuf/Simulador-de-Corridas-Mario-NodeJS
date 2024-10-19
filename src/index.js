18f71491f12a4c15fd47b6ecd44df50e5ecaa386
André Luiz <andre.luiz.a.f@hotmail.com>
2024-10-19 10:02:41 -0300

    Roll Dice(Async Functions)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 // Definindo os jogadores
 const player1 = {
-    Nome: "Mario", // Nome do jogador 1
-    VELOCIDADE: 4,   // Velocidade do Mario
-    MANOBRABILIDADE: 3, // Nível de habilidade de manobras do Mario
-    PODER: 3,         // Nível de poder do Mario
-    PONTOS: 0        // Pontuação inicial do Mario
+  Nome: "Mario", // Nome do jogador 1
+  VELOCIDADE: 4, // Velocidade do Mario
+  MANOBRABILIDADE: 3, // Nível de habilidade de manobras do Mario
+  PODER: 3, // Nível de poder do Mario
+  PONTOS: 0, // Pontuação inicial do Mario
 };
 
 const player2 = {
-    Nome: "Luigi", // Nome do jogador 2
-    VELOCIDADE: 5,   // Velocidade do Luigi
-    MANOBRABILIDADE: 2, // Nível de habilidade de manobras do Luigi
-    PODER: 2,         // Nível de poder do Luigi
-    PONTOS: 0        // Pontuação inicial do Luigi
+  Nome: "Luigi", // Nome do jogador 2
+  VELOCIDADE: 5, // Velocidade do Luigi
+  MANOBRABILIDADE: 2, // Nível de habilidade de manobras do Luigi
+  PODER: 2, // Nível de poder do Luigi
+  PONTOS: 0, // Pontuação inicial do Luigi
+};
+
+// Função para rolar o dado
+async function rollDice() {
+    return Math.floor(Math.random() * 6) + 1; // Gera um número aleatório entre 1 e 6
 }
